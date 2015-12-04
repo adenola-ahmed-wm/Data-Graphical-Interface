e1256ca93a3831fe9b73eee31b62e1ac94f699fe
Start Coding Student 2 <session2@podium3mac.west-mec.local>
2015-12-03 18:00:13 -0700

    added js

--- Data-Graphical-Interface.js
+++ Data-Graphical-Interface.js
@@ -1,31 +1,34 @@
 /**
  * Created by session2 on 10/7/15.
  */
-googlle.load('visualization', '1', {packages: ['corechart', 'bar']});
-googlle.setOnLoadCallback(drawBasic);
+google.load('visualization', '1', {
+    packages: ['corechart', 'bar']
+});
+google.setOnLoadCallback(drawBasic);
 
 function drawBasic() {
 
     var data = google.visualization.arrayToDataTable([
-        ['City', '2010 Population', { role: 'style' }],
-        ['New York City, NY', 8175000, 'color: red' ],
-        ['Los Angeles, CA', 3792000, 'color: gold'],
-        ['Chicago, IL', 2695000, 'color: silver'],
-        ['Houston, TX', 2099000, 'color: orange'],
-        ['Philadelphia, PA', 1526000, 'color: black']
+        ['Rapper', 'Net worth in millions', ],
+        ['Dr.Dre', 810, 000, 000],
+        ['Los Angeles, CA', 3792000],
+        ['Chicago, IL', 2695000],
+        ['Houston, TX', 2099000],
+        ['Philadelphia, PA', 1526000]
     ]);
 
     var options = {
-        title: 'Population of Largest U.S. Cities',
-        chartArea: {width: '50%'},
+        title: 'Top 10 Richest Rappers',
+        chartArea: {
+            width: '30%'
+        },
         hAxis: {
-            title: 'Total Population',
+            title: 'Net worth in millions',
             minValue: 0
         },
         vAxis: {
-            title: 'City'
-        },
-        backgroundColor: 'blue'
+            title: 'Rappers'
+        }
     };
 
     var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
